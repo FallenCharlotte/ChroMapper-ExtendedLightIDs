82402441bee9e568e462412940935472705672ac
Moizac <moistsac@gmail.com>
2023-05-14 19:44:27 -0700

    Skips

--- ChroMapper-ExtendedLightIDs/Plugin.cs
+++ ChroMapper-ExtendedLightIDs/Plugin.cs
@@ -12,83 +12,66 @@ namespace ChroMapper_MoarLightIDs {
 [Plugin("Extended LightIDs")]
 public class Plugin {
 	private PlatformDescriptor descriptor;
+	private Dictionary<int, SortedSet<int>> ids = new Dictionary<int, SortedSet<int>>();
 	
 	[Init]
 	private void Init() {
 		LoadInitialMap.PlatformLoadedEvent += PlatformLoaded;
-		LoadInitialMap.LevelLoadedEvent += AddIds;
-		CMInputCallbackInstaller.InputInstance.EventGrid.ToggleLightPropagation.performed += Update;
-		CMInputCallbackInstaller.InputInstance.EventGrid.CycleLightPropagationUp.performed += Update;
-		CMInputCallbackInstaller.InputInstance.EventGrid.CycleLightPropagationDown.performed += Update;
-		CMInputCallbackInstaller.InputInstance.EventGrid.ToggleLightIdMode.performed += Update;
+		LoadInitialMap.LevelLoadedEvent += RefreshIDs;
+		CMInputCallbackInstaller.InputInstance.EventGrid.ToggleLightPropagation.performed += (_) => RefreshIDs();
+		CMInputCallbackInstaller.InputInstance.EventGrid.CycleLightPropagationUp.performed += (_) => RefreshIDs();
+		CMInputCallbackInstaller.InputInstance.EventGrid.CycleLightPropagationDown.performed += (_) => RefreshIDs();
+		CMInputCallbackInstaller.InputInstance.EventGrid.ToggleLightIdMode.performed += (_) => RefreshIDs();
 	}
 	
 	public void PlatformLoaded(PlatformDescriptor descriptor) {
 		this.descriptor = descriptor;
 	}
 	
-	public void AddIds() {
-		var max_ids = new Dictionary<int, int>();
-		// Get max from Environment Enhancements
-		
+	public SortedSet<int> IDsForType(int type) {
+		if (!ids.ContainsKey(type)) {
+			var lm = descriptor.LightingManagers[type];
+			ids.Add(type, new SortedSet<int>(lm.LightIDPlacementMap.Values));
+		}
+		return ids[type];
+	}
+	
+	public void RefreshIDs() {
+		// Get IDs from Environment Enhancements, V3 only
 		foreach (var eh in BeatSaberSongContainer.Instance.Map.EnvironmentEnhancements) {
-			if (eh.LightID is int LightID) {
+			if (eh.LightID is int id) {
 				int type = (eh.Components["ILightWithId"].HasKey("type"))
 					? eh.Components["ILightWithId"]["type"].AsInt
-					: GuessType(eh);
+					: -1;
 				if (type < 0) {
 					continue;
 				}
-				int old_max = 0;
-				max_ids.TryGetValue(type, out old_max);
-				max_ids[type] = System.Math.Max(old_max, LightID);
+				IDsForType(type).Add(id);
 			}
 		}
 		
-		// Get max from events
+		// Get IDs from events
 		foreach (var o in BeatmapObjectContainerCollection.GetCollectionForType(Beatmap.Enums.ObjectType.Event).UnsortedObjects) {
 			var ev = (BaseEvent)o;
 			if (ev.CustomLightID != null) {
-				int old_max = 0;
-				max_ids.TryGetValue(ev.Type, out old_max);
-				max_ids[ev.Type] = System.Math.Max(old_max, ev.CustomLightID.Max());
+				IDsForType(ev.Type).UnionWith(ev.CustomLightID);
+				Debug.Log(string.Join(",", IDsForType(ev.Type).ToList()));
 			}
 		}
 		
 		// Add them all
-		foreach (var ids in max_ids) {
-			var lm = descriptor.LightingManagers[ids.Key];
-			Debug.Log($"Adding type {ids.Key} from {lm.LightIDPlacementMapReverse.Count} to {ids.Value}");
-			for (int i = lm.LightIDPlacementMapReverse.Count + 1; i <= ids.Value; ++i) {
-				lm.LightIDPlacementMap.Add(i - 1, i);
-				lm.LightIDPlacementMapReverse.Add(i, i - 1);
+		foreach (var type in ids) {
+			var lm = descriptor.LightingManagers[type.Key];
+			
+			var i = 0;
+			lm.LightIDPlacementMap.Clear();
+			lm.LightIDPlacementMapReverse.Clear();
+			foreach (var id in type.Value) {
+				lm.LightIDPlacementMap.Add(i, id);
+				lm.LightIDPlacementMapReverse.Add(id, i);
+				++i;
 			}
 		}
-	}
-	
-	public void Update(InputAction.CallbackContext context) {
-		AddIds();
-	}
-	
-	public int GuessType(Beatmap.Base.Customs.BaseEnvironmentEnhancement eh) {
-		string platform = descriptor.gameObject.name.Replace("(Clone)", "");
-		if (eh.ID.Contains("Color")) {
-			return 0;
-		}
-		if (eh.ID.Contains("Ring") && platform != "Kaleidoscope") {
-			return 1;
-		}
-		if (eh.ID.Contains("BaseL")) {
-			return 2;
-		}
-		if (eh.ID.Contains("BaseR")) {
-			return 3;
-		}
-		if (eh.ID.Contains("Front")) {
-			return 4;
-		}
-		Debug.Log("Couldn't guess event type from '{eh.ID}' platform {platform}");
-		return -1;
 	}
 	
 	[Exit]
