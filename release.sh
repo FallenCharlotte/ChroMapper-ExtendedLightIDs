03f53ce0113d69dfc8ab772997779b731568433c
Moizac <moistsac@gmail.com>
2024-02-22 20:51:48 -0800

    Update build tools and support UpdateChecker

--- release.sh
+++ release.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 PROJECT="$(basename "$(pwd)")"
+CHANGELOG="Changelog.txt"
 ver=$1
 short=$1
 
@@ -11,6 +12,22 @@ done
 
 sed -i 's/AssemblyVersion(.*)/AssemblyVersion("'$ver'")/' $PROJECT/Properties/AssemblyInfo.cs
 sed -i 's/AssemblyFileVersion(.*)/AssemblyFileVersion("'$ver'")/' $PROJECT/Properties/AssemblyInfo.cs
+sed -i 's/"version": ".*",/"version": "v'$short'",/' $PROJECT/manifest.json
+
+mv "$CHANGELOG"{,.old}
+echo "v${short}" > "$CHANGELOG"
+git log $(git describe --tags --abbrev=0)..HEAD --pretty=format:'	%s' >> "$CHANGELOG"
+echo -e "\n" >> "$CHANGELOG"
+cat "$CHANGELOG".old >> "$CHANGELOG"
+$EDITOR "$CHANGELOG"
+_status=$?
+if [[ $_status != 0 ]]; then
+	echo "Aborting..."
+	rm "$CHANGELOG"
+	mv "$CHANGELOG"{.old,}
+	exit
+fi
+rm "$CHANGELOG".old
 
 git add $PROJECT/Properties/AssemblyInfo.cs
 git commit -m "v${short}"
